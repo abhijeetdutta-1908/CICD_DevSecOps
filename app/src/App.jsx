e4108926942bd5eafec178ddadeea49db5fce330
Abhijeet <abhijeetduttaam@gmail.com>
2025-07-21 16:17:42 +0530

    modified the deployment.yaml

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -28,8 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more 
         hello it me
-        lolll
-        hgfyghj
+      
       </p>
     </>
   )
